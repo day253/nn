34c1764d17ea24663f173928affc62967968368b
day253 <9634619+day253@users.noreply.github.com>
2023-03-10 23:01:29 +0800

    add init

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 class neuralNetwork:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
 
     def train():
         pass
 
     def query():
         pass
+
+
+if __name__ == "__main__":
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+    learning_rate = 0.3
+
+    n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
